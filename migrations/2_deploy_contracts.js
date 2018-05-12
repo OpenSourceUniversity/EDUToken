4885e587141ad274c6d8a7c4d37604779fc00bd1
Dobromir Kovachev <dobromir.mail@gmail.com>
2018-05-12 15:04:08 +0300

    Fix deploy_contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,5 +2,15 @@
 var EDUToken = artifacts.require("./EDUToken.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(EDUToken);
+  // var startDate = Math.floor(Date.now()/1000);
+
+  deployer.deploy(EDUToken,
+  	web3.eth.accounts[0],
+  	web3.eth.accounts[1],
+  	web3.eth.accounts[2],
+  	web3.eth.accounts[3],
+  	web3.eth.accounts[4],
+    web3.eth.accounts[5],
+    1150
+  );
 };
