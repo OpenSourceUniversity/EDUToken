5d2b8a80714d7558c8f69afefcbdbf1f6b2fbaab
Maxim Dimitrov <maxim.dimitrov@os.university>
2018-05-15 17:37:16 +0300

    local testing files

--- test/01_crowdsale.js
+++ test/01_crowdsale.js
@@ -1,19 +1,19 @@
-import assertRevert from '../../helpers/assertRevert';
-import { inLogs } from '../../helpers/expectEvent';
-import Promise from 'bluebird';
-import time from '../helpers/time';
-
-// Test
-
-var EDUToken = artifacts.require("./EDUToken.sol");
-
-
-
-var Promise = require('bluebird')
-var time = require('../helpers/time')
-
-import shouldBehaveLikeBurnableToken from './BurnableToken.behaviour';
-
-require('chai').use(require('chai-as-promised'))
-  .use(require('chai-bignumber')(BigNumber))
-  .should();
+// import assertRevert from '../../helpers/assertRevert';
+// import { inLogs } from '../../helpers/expectEvent';
+// import Promise from 'bluebird';
+// import time from '../helpers/time';
+//
+// // Test
+//
+// var EDUToken = artifacts.require("./EDUToken.sol");
+//
+//
+//
+// var Promise = require('bluebird')
+// var time = require('../helpers/time')
+//
+// import shouldBehaveLikeBurnableToken from './BurnableToken.behaviour';
+//
+// require('chai').use(require('chai-as-promised'))
+//   .use(require('chai-bignumber')(BigNumber))
+//   .should();
---
+++ test/TestEDUToken.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.23;
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/EDUToken.sol";
+
+contract TestEDUToken {
+
+    function testInitialBalanceUsingDeployedContract() public {
+        EDUToken edu = EDUToken(DeployedAddresses.EDUToken());
+
+        uint expected = 48000000;
+
+        Assert.equal(edu.balanceOf(tx.origin), expected, "Owner should have 10000 EDU initially");
+    }
+
+}
