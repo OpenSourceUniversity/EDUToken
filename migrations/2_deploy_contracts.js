8f797740bf6bc66e5872cd12b45b5bc24f272972
Jordan Jambazov <jordan.jambazov@os.university>
2018-05-24 23:37:42 +0300

    Get deploy arguments from env

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,13 +4,13 @@ var EDUCrowdsale = artifacts.require("./EDUCrowdsale.sol");
 module.exports = function(deployer) {
   var certifierAddresses = {
     "kovan": "0x5082a3716b8e5f2b164fffc38c5f0ae36a179974",
-    "mainnet": "0x7c465059d3288cf502d2c3cc32db825c2cfc80ba",
+    "mainnet": process.env.CERTIFIER,
   }
   var certifier = certifierAddresses[
     process.env.DEPLOY_PRODUCTION == "true" ? "mainnet" : "kovan"
   ];
-  var wallet = "0xFaa1447B9Ae34C3893b486b61906B5415106eF57",
-      tokenWallet = "0xFaa1447B9Ae34C3893b486b61906B5415106eF57",
+  var wallet = process.env.FUNDS_WALLET,
+      tokenWallet = process.env.TOKEN_WALLET,
       cap = 34000 * (10 ** 18),
       openingTime = 1528113600,
       closingTime = 1530446400;
